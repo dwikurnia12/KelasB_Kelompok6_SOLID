bdbba48b029bba1d4d6e04761100a9c81d689377
BIma <arofakarindrabimanta@student.uns.ac.id>
2026-05-25 22:37:03 +0700

    Update after.py

--- src/ocp/after.py
+++ src/ocp/after.py
@@ -1,6 +1,5 @@
 from abc import ABC, abstractmethod
 
-# Class induk diabstraksikan agar Open for Extension
 class Hewan(ABC):
     def __init__(self, nama):
         self.nama = nama
@@ -13,7 +12,6 @@ class Hewan(ABC):
     def bergerak(self):
         pass
 
-# Mengembangkan jenis hewan baru tanpa mengubah class utama
 class Burung(Hewan):
     def makan(self):
         print(f"{self.nama} sedang makan.")
@@ -28,7 +26,6 @@ class Singa(Hewan):
     def bergerak(self):
         print(f"{self.nama} sedang berlari.")
 
-# Class KebunBinatang menjadi Closed for Modification
 class KebunBinatang:
     def __init__(self, kandang):
         self.kandang = kandang
@@ -36,4 +33,4 @@ class KebunBinatang:
     def rawat_semua_hewan(self):
         for hewan in self.kandang.hewan_list:
             hewan.makan()
-            hewan.bergerak() # Polimorfisme secara otomatis memanggil cara bergerak tiap hewan
+            hewan.bergerak()
